9e5075344129a863d20caca24a9aab69e92ad796
Pablo Araujo <pabloaraujo@Pablos-MacBook-Pro.local>
2019-04-02 19:51:19 -0300

    sexto commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.NODE_PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, err => {
   err ? console.log('ERROR', err) : console.log(`server running on port ${PORT}`);
